be17e9b22a0d2243b1a9e84053f6283f06eef34e
Geoffroy Perriard <geoffroy.perriard@live.fr>
2012-05-01 13:38:09 +0200

    Add default role ROLE_MEMBER when a user registers in system

---
+++ src/Btask/UserBundle/Model/OnsiteUserManager.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Btask\UserBundle\Model;
+
+use FOS\UserBundle\Entity\UserManager;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+
+class OnsiteUserManager extends UserManager
+{    
+    /**
+     * Returns an user instance with ROLE_MEMBER
+     *
+     * @return UserInterface
+     */
+    public function createUser()
+    {
+        $class = $this->getClass();
+        $user = new $class;
+
+        // Add ROLE_MEMBER to user who registers
+        $user->addRole('ROLE_MEMBER');
+
+        return $user;
+    }
+
+    /**
+     * Finds a user by username or email
+     *
+     * @param string $username
+     * @return UserInterface
+     */
+    public function loadUserByUsername($username)
+    {
+        $user = $this->findUserByUsernameOrEmail($username);
+
+        if (!$user) {
+            throw new UsernameNotFoundException(sprintf('No user with name "%s" was found.', $username));
+        }
+
+        return $user;
+    }
+}
