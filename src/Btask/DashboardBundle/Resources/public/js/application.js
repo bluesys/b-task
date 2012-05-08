55f68bc65d79c1c9348f5225fb94458ba1214a89
Geoffroy Perriard <geoffroy.perriard@live.fr>
2012-05-08 23:10:10 +0200

    Align the content on the mobile version

--- src/Btask/DashboardBundle/Resources/public/js/application.js
+++ src/Btask/DashboardBundle/Resources/public/js/application.js
@@ -2,4 +2,8 @@ $(function() {
 	// Active the dropdown method
 	$('.dropdown-toggle').dropdown();
 
+	// Load the dummies data in the typeahead input
+	var alCities = ['Baltimore', 'Boston', 'New York', 'Tampa Bay', 'Toronto', 'Chicago', 'Cleveland', 'Detroit', 'Kansas City', 'Minnesota', 'Los Angeles', 'Oakland', 'Seattle', 'Texas'].sort();
+	$('.underling').typeahead({source: alCities, items:5});
+
 });
