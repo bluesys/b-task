8ea9776371c1aed5dfc9b79b01910b6450d58fbc
Geoffroy Perriard <geoffroy.perriard@live.fr>
2012-05-08 23:57:48 +0200

    Add margin and padding beetween elements on the mobile version

--- src/Btask/DashboardBundle/Resources/public/js/application.js
+++ src/Btask/DashboardBundle/Resources/public/js/application.js
@@ -2,8 +2,7 @@ $(function() {
 	// Active the dropdown method
 	$('.dropdown-toggle').dropdown();
 
-	// Load the dummies data in the typeahead input
+	// Load the dummies data in the typeahead
 	var alCities = ['Baltimore', 'Boston', 'New York', 'Tampa Bay', 'Toronto', 'Chicago', 'Cleveland', 'Detroit', 'Kansas City', 'Minnesota', 'Los Angeles', 'Oakland', 'Seattle', 'Texas'].sort();
 	$('.underling').typeahead({source: alCities, items:5});
-
 });
