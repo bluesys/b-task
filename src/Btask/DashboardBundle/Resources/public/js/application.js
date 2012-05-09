c36f921773e5c661bfb4fa89b6f783b95b3b5c05
Geoffroy Perriard <geoffroy.perriard@live.fr>
2012-05-09 10:25:41 +0200

    Add a button to hide the dashboard on mobile version

--- src/Btask/DashboardBundle/Resources/public/js/application.js
+++ src/Btask/DashboardBundle/Resources/public/js/application.js
@@ -6,4 +6,17 @@ $(function() {
 	// Load the dummies data in the typeahead
 	var alCities = ['Baltimore', 'Boston', 'New York', 'Tampa Bay', 'Toronto', 'Chicago', 'Cleveland', 'Detroit', 'Kansas City', 'Minnesota', 'Los Angeles', 'Oakland', 'Seattle', 'Texas'].sort();
 	$('.underling').typeahead({source: alCities, items:5});
+
+	// Hide the dashboard on click in the nav button (only for mobile)
+	var dashboardToggle = false;
+	$('.dashboard-toggle a').click(function () {
+		if (dashboardToggle == false) {
+			$('#dashboard').css({ display: "none" });
+			dashboardToggle = true;
+		}
+		else {
+			$('#dashboard').css({ display: "block" });
+			dashboardToggle = false;
+		}
+	});
 });
