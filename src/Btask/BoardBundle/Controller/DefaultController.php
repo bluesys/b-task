e692a441d1e433fbaa0b8b3d2443e6131f91c2d9
Geoffroy Perriard <geoffroy.perriard@live.fr>
2012-06-05 00:55:48 +0200

    Add informations on project view

--- src/Btask/BoardBundle/Controller/DefaultController.php
+++ src/Btask/BoardBundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
+use Btask\BoardBundle\Entity\Project;
+
 class DefaultController extends Controller
 {
     public function showBoardAction()
@@ -20,8 +22,33 @@ class DefaultController extends Controller
         return $this->render('BtaskBoardBundle:Default:today.html.twig');
     }
 
-    public function showProjectAction()
+	/**
+	 * Display the project view
+	 * 
+	 */
+    public function showProjectAction($project_slug)
     {
-        return $this->render('BtaskBoardBundle:Default:project.html.twig');
+		$request = $this->container->get('request');
+		if(!$request->isXmlHttpRequest()) {
+			throw new NotFoundHttpException();
+		}
+
+		$user = $this->get('security.context')->getToken()->getUser();
+
+		// Get the project
+		$em = $this->getDoctrine()->getEntityManager();
+		$project = $em->getRepository('BtaskBoardBundle:Project')->findOneBySlug($project_slug);
+		
+		if(!project) {
+			throw new NotFoundHttpException();
+		}
+		
+		if(!project->isSharedTo($user)) {
+            throw new AccessDeniedHttpException();
+		}
+		
+        return $this->render('BtaskBoardBundle:Default:project.html.twig', array(
+			'project' => $project,
+        ));
     }
 }
